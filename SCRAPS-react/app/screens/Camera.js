5d0f09b8571f3cfeb1abdf53396419801700b72d
[Rushil Aggarwal] <[rushilaggarwal150@gmail.com]>
2024-02-22 18:01:40 -0600

    Added Camera and Camera Roll permissions

--- SCRAPS-react/app/screens/Camera.js
+++ SCRAPS-react/app/screens/Camera.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Camera, CameraType} from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
-export default function Home() {
+export default function CameraScreen() {
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const[image, setImage] = useState(null);
+    const[type, setType] = useState(Camera.Constants.Type.baclk);
+    const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+    const cameraRef = useRef(null);
+
+    useEffect(() => {
+        (async () => {
+            MediaLibrary.requestPermissionsAsync();
+            const cameraStatus = await Camera.requestCameraPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+        })();
+    }, [])
     return (
         <View style={styles.container}>
             <View style={styles.header}>
