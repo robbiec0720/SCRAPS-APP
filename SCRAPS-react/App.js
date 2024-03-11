a96aa5f1d2a21330514b80cd111715cc2af25cf2
Robbie Clark <robbiec0720@gmail.com>
2024-03-11 15:36:12 -0500

    added pref screen

--- SCRAPS-react/App.js
+++ SCRAPS-react/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { ImageProvider } from './app/context/imagecontext';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Tabs from './app/component/tab';
+import Preference from './app/screens/Preference';
+
+const Stack = createStackNavigator();
+
+export function HomeTabs() {
+  return (
+    <ImageProvider>
+      <Tabs/>
+    </ImageProvider>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ImageProvider>
-        <Tabs/>
-      </ImageProvider>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component={HomeTabs} options={{headerShown:false}}></Stack.Screen>
+        <Stack.Screen name = "Preference" component={Preference} options={{headerShown:false}}></Stack.Screen> 
+      </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
