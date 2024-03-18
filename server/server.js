84d1cb6df148ee2027ab2b23865f9ff970524fde
[Rushil Aggarwal] <[rushilaggarwal150@gmail.com]>
2024-03-17 20:55:25 -0500

    Update Diet restrictions

--- server/server.js
+++ server/server.js
@@ -38,51 +38,59 @@ const dbpool = new Pool({
     ssl: {rejectUnauthorized: false}
 });
 
-let recipes = []
-app.get('/recipes', (req, res) => {
-    dbpool
-        .query('SELECT * FROM recipes;')
-        .then(query_res => {
-            for (let i = 0; i < query_res.rowCount; i++) {
-                recipes.push(query_res.rows[i]);
-            }
-            const data = {recipes};
-            //console.log(recipes);
-            res.json(data);
-        }).catch(err => console.log(err));
-});
 
-let filter_recipe = recipes;
-app.post('/filter', (req, res) => {
-    const{vegan, vegetarian, lactose, gluten, halal, kosher, nut, shellfish, pescatarian} = req.body;
-    if(vegan === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.vegan);
-    }
-    if(vegetarian === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.vegetarian);
-    }
-    if(lactose === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.lactose);
-    }
-    if(gluten === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.gluten);
-    }
-    if(halal === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.halal);
-    }
-    if(kosher === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.kosher);
-    }
-    if(nut === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.nut);
-    }
-    if(shellfish === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.shellfish);
-    }
-    if(pescatarian === true){
-        filter_recipe = filter_recipe.filter(filter_recipe => filter_recipe.pescatarian);
-    }
-    const data = {filter_recipe};
-    console.log(filter_recipe);
-    res.json(data);
+app.get('/recipes', (req, res) => {
+    let recipes = []
+    //const {username} = req.body;
+    username = 'xhuddini';
+    Promise.all([
+        dbpool.query('SELECT * FROM recipes LIMIT 10;'),
+        dbpool.query('SELECT * FROM users WHERE username = $1::text;', [username])
+    ]).then(([recipesResult, usersResult]) => {
+        for (let i = 0; i < recipesResult.rowCount; i++) {
+            recipes.push(recipesResult.rows[i]);
+        }
+        let user = usersResult.rows;
+
+        if (row.length === 0) {
+            res.status(401).send("Invalid username.");
+            return;
+        }
+        
+        user = user[0];
+        //console.log(user.vegan);
+        if(user.vegan === true){
+            recipes = recipes.filter(recipes => recipes.vegan);
+        }
+        if(user.vegetarian === true){
+            recipes = recipes.filter(recipes => recipes.vegetarian);
+        }
+        if(user.lactose === true){
+            recipes = recipes.filter(recipes => recipes.lactose);
+        }
+        if(user.gluten === true){
+            recipes = recipes.filter(recipes => recipes.gluten);
+        }
+        if(user.halal === true){
+            recipes = recipes.filter(recipes => recipes.halal);
+        }
+        if(user.kosher === true){
+            recipes = recipes.filter(recipes => recipes.kosher);
+        }
+        if(user.nut === true){
+            recipes = recipes.filter(recipes => recipes.nut);
+        }
+        if(user.shellfish === true){
+            recipes = recipes.filter(recipes => recipes.shellfish);
+        }
+        if(user.pescatarian === true){
+            recipes = recipes.filter(recipes => recipes.pescatarian);
+        }
+        console.log(user);
+        const data = {recipes};
+        res.json(data);
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred' });
+    });
 });
